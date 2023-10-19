15d7b42d430748cf34ec452f8077bd4b24420378
Cojomax99 <cojomax99@gmail.com>
2023-10-18 23:02:18 -0400

    Add Manatee
    Closes #561.
    
    Co-authored-by: Gegy <gegy.dev@gmail.com>

--- src/main/java/net/tropicraft/core/client/TropicraftRenderLayers.java
+++ src/main/java/net/tropicraft/core/client/TropicraftRenderLayers.java
@@ -69,6 +69,9 @@ public class TropicraftRenderLayers {
     public static ModelLayerLocation SPIDER_MONKEY_LAYER;
     public static ModelLayerLocation WHITE_LIPPED_PECCARY_LAYER;
     public static ModelLayerLocation GIBNUT_LAYER;
+    public static ModelLayerLocation MANATEE_LAYER;
+
+    // Scuba
     public static ModelLayerLocation CHEST_SCUBA_LAYER;
     public static ModelLayerLocation FEET_SCUBA_LAYER;
     public static ModelLayerLocation HEAD_SCUBA_LAYER;
@@ -123,6 +126,7 @@ public class TropicraftRenderLayers {
         SPIDER_MONKEY_LAYER = registerMain("spider_monkey", SpiderMonkeyModel::create, event);
         WHITE_LIPPED_PECCARY_LAYER = registerMain("white_lipped_peccary", WhiteLippedPeccaryModel::create, event);
         GIBNUT_LAYER = registerMain("gibnut", GibnutModel::create, event);
+        MANATEE_LAYER = registerMain("manatee", ManateeModel::create, event);
 
         HEADPIECE_LAYER = registerMain("headpiece", PlayerHeadpieceModel::create, event);
         HEAD_SCUBA_LAYER = registerMain("scuba_goggles", ModelScubaGear::create, event);
